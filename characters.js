75b8a041b6b58a73c0a867705fbebae8f962f712
Chris Parravicini <chris.parravicini@gmail.com>
2022-03-07 00:08:35 -0300

    pf primera entrega

---
+++ characters.js
@@ -0,0 +1,34 @@
+const characters = [];
+
+const names = () => {
+
+    let nameC = prompt('Pick a name for your character')
+    let powerC = prompt('Pick a power for your character')
+    let colorC = prompt('Pick a color for your character')
+
+    const characterPicked = new Character (nameC, powerC, colorC)
+
+    console.log(characterPicked);
+
+
+    function Character( name, power, color ) {
+        this.name = name;
+        this.power = power;
+        this.color = color;
+    
+    }
+
+    characters.push(characterPicked)
+
+    const colores = characters.find((x) => x.color === "red")
+    console.log(colores);
+
+    const colors = characters.filter((x) => x.color === 'yellow')
+    console.log(colors);
+
+
+}
+
+names()
+names()
+
