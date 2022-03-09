8627657d06abd2322a7e005607b5bf6adbde7589
Chris Parravicini <chris.parravicini@gmail.com>
2022-03-08 23:51:00 -0300

    DOM 3-9-22

--- characters.js
+++ characters.js
@@ -1,40 +1,50 @@
-let characters = [];
-console.log(characters.length);
 
-const names = () => {
+function saludar () {
+    alert ('Welcome to Fictionized');
+    console.log('Welcome');
+}
 
-    let nameC = prompt('Pick a name for your character')
-    let powerC = prompt('Pick a power for your character')
-    let colorC = prompt('Pick a color for your character')
+saludar();
 
-    const characterPicked = new Character (nameC, powerC, colorC)
+const title = document.querySelector('#title')
+title.innerText = 'Welcome to Fictionized'
 
-    console.log(characterPicked);
+const ppl = document.getElementById('ppl')
+
+const newLi = document.createElement('li')
+newLi.innerText = "Character two";
+ppl.append(newLi)
 
 
-    function Character( name, power, color ) {
+function Character( name, power, color ) {
         this.name = name;
         this.power = power;
         this.color = color;
     
-    }
-
-    characters.push(characterPicked)
-
-    const colores = characters.find((x) => x.color === "red")
-    console.log(colores);
-
-    const colors = characters.filter((x) => x.color === 'yellow')
-    console.log(colors);
-
-
 }
 
-names()
-names()
+let characters = [];
 
+let character = () => {
 
-const title = document.querySelector('#title')
-console.log(title.innerText);
-title.innerText = 'Welcome to Fictionized'
+    let nameC = prompt('Pick a name for your character')
+    let powerC = prompt('Pick a power for your character')
+    let colorC = prompt('Pick a color for your character')
+    
+    const characterPicked = new Character (nameC, powerC, colorC)
+    
+    console.log(characterPicked);
+
+    characters.push(characterPicked)
+    
+    }
+
+character()
+character()
+
+const colores = characters.find((x) => x.color === "red")
+console.log(colores);
+
+const colors = characters.filter((x) => x.color === 'yellow')
+console.log(colors);
 
