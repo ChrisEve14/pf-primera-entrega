decae05c695eaf4bc98eb6754789c2aa1acf54fe
Chris Parravicini <chris.parravicini@gmail.com>
2022-03-17 00:40:26 -0300

    avances

--- characters.js
+++ characters.js
@@ -1,58 +1,78 @@
 
-function saludar () {
-    alert ('Welcome to Fictionized');
-    console.log('Welcome');
-}
+// function saludar () {
+//     alert ('Welcome to Fictionized');
+//     console.log('Welcome');
+// }
 
-saludar();
+// saludar();
 
 const title = document.querySelector('#title')
 title.innerText = 'Welcome to Fictionized'
+
+//consola de eleccion
+
+const storedCharacters = localStorage.getItem('characters')
+let characters = []
+console.log(characters);
+console.log(localStorage.getItem('characters'));
+
+if(storedCharacters) {
+    characters = JSON.parse(storedCharacters);
+    const addOldLi = document.createElement('li')
+    addOldLi.innerText = characters
+    ppl.append(addOldLi)
+
+}
+
 
 const btn = document.getElementById('botonUno')
 
 const entrada = document.getElementById('entradaUno')
 entrada.oninput = () => {
-    console.log('Ingreso Nombre');
+    console.log('Ingreso Nombre');  
 }
-
-btn.onclick = ()=>{
-    console.log(entrada.value);
-
-    const ppl = document.getElementById('ppl')
-
-    const newLi = document.createElement('li')
-    newLi.innerText = `${entrada.value}`;
-    ppl.append(newLi)
-}
-
-const btnone = document.getElementById('botonDos')
 
 const entradau = document.getElementById('entradaDos')
 entradau.oninput = () => {
     console.log('Selecciono opcion');
 }
-
-btnone.onclick = ()=>{
-    console.log(entradau.value);
-}
-
-const btntwo = document.getElementById('botonTres')
-
 const entradad = document.getElementById('entradaTres')
 entradad.oninput = () => {
     console.log('Ingreso Color');
 }
 
-btntwo.onclick = ()=>{
+btn.onclick = ()=>{
+    console.log(entrada.value);
+    console.log(entradau.value);
     console.log(entradad.value);
 
+    const ppl = document.getElementById('ppl')
+
+    const newLi = document.createElement('li')
+    newLi.innerText = `${entrada.value.toUpperCase()} - ${entradau.value.toUpperCase()} - ${entradad.value.toUpperCase()} `;
+    ppl.append(newLi)
+
+
+   
+    let contenido = newLi.innerText
+    console.log(contenido);
+    characters.push(contenido);
+    localStorage.setItem('characters', JSON.stringify(characters))
+    
     const color = [entradad.value]
 
-    const encontrar = color.find((x) => `${entradad.value}` === 'Red')
+    const encontrar = color.find((x) => `${entradad.value.toUpperCase()}` === 'RED')
     console.log(encontrar);
 
-    const filtrar = color.filter((x) => `${entradad.value}` === 'Yellow')
+    const filtrar = color.filter((x) => `${entradad.value.toUpperCase()}` === 'YELLOW')
     console.log(filtrar);
+
+    entrada.value = '';
+    entradau.value = '';
+    entradad.value = '';
+
 }
+
+
+
 
